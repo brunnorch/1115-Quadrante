b1c63a35136a3271271b9f484518f7b03157945c
Brunno Rocha <111777185+brunnorch@users.noreply.github.com>
2022-10-05 16:30:10 -0300

    Quadrante

---
+++ main.py
@@ -0,0 +1,28 @@
+x = int(input("1º codernada: "))
+y = int(input("2º codernada: "))
+
+if x > 0 and y > 0:
+    print("Primeiro\n")
+elif x > 0 and y < 0:
+    print("Quarto\n")
+elif x < 0 and y < 0:
+    print("Terceiro\n")
+elif x < 0 and y > 0:
+    print("Segundo\n")
+else:
+    print("")
+
+while x != 0 and y != 0:
+    x = int(input("1º codernada: "))
+    y = int(input("2º codernada: "))
+
+    if x > 0 and y > 0:
+        print("Primeiro\n")
+    elif x > 0 and y < 0:
+        print("Quarto\n")
+    elif x < 0 and y < 0:
+        print("Terceiro\n")
+    elif x < 0 and y > 0:
+        print("Segundo\n")
+    else:
+        print("")
